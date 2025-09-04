c740978128e7532eee550ee74562ad677a8a41c7
SRIRAM <srirammurugesan1807@gmail.com>
2025-09-04 10:49:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.users);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.firstName} {user.lastName}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const UserDetails = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{user.firstName} {user.lastName}</h2>
+      <p><b>Email:</b> {user.email}</p>
+      <p><b>Phone:</b> {user.phone}</p>
+      <p><b>Age:</b> {user.age}</p>
+      <Link to="/">Back to User List</Link>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/users/:id" element={<UserDetails />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
